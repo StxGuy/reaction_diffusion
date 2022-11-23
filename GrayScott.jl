6a39354f8159d16c01320c0d65cde74327736b01
Prof. Carlo R. da Cunha, Ph.D <carlo.requiao@gmail.com>
2022-11-22 22:39:03 -0300

    Create GrayScott.jl

---
+++ GrayScott.jl
@@ -0,0 +1,46 @@
+# Brian Silverman's Wireworld
+
+using Plots
+
+function ∇²(f)
+    m = -f + 0.2*(circshift(f,(0,1)) + circshift(f,(0,-1)) + circshift(f,(1,0)) + circshift(f,(-1,0)))
+    m += 0.05*(circshift(f,(1,1)) + circshift(f,(1,-1)) + circshift(f,(-1,1)) + circshift(f,(-1,-1)))
+       
+    return m
+end
+
+# Transition rules
+function action(u,v)
+    Ny,Nx = size(u)
+    ∇²u = zeros(Ny,Nx)
+    ∇²v = zeros(Ny,Nx)
+
+    F = 0.034
+    k = 0.095
+    r = 1
+    Du = 1
+    Dv = 0.5
+    
+    dt = 0.1
+    
+    uv² = u.*(v.^2)
+    
+    un = u + (Du*∇²(u) - r*uv² + F*(1 .- u))*dt
+    vn = v + (Dv*∇²(v) + r*uv² - (F + k)*v)*dt
+    
+    return un,vn
+end
+
+#===============================#
+A = ones(101,101)
+B = zeros(101,101)
+B[45:55,45:55] .= 1
+ 
+anim = @animate for i = 1:10000
+    global A,B = action(A,B)
+    heatmap(B./(A+B),c=:redsblues)
+end
+
+gif(anim, "test.gif", fps = 50)
+    
+            
